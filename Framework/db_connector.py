8d5cecf0b1180eb0eb3d7ba7b2627a99dc3262d9
MRStelio <mbelive@rambler.ru>
2023-10-10 22:52:06 +0500

    Добавлено подключение к БД, создан .env файл

---
+++ Framework/db_connector.py
@@ -0,0 +1,47 @@
+import os
+import mysql.connector
+from dotenv import load_dotenv
+
+
+class DatabaseConnector:
+
+    def __init__(self):
+        """Соединение с БД."""
+
+        load_dotenv()
+        self.db = mysql.connector.connect(
+          host=os.getenv('DB_HOST'),
+          user=os.getenv("DB_USER"),
+          password=os.getenv("DB_PASSWORD"),
+          database=os.getenv("DB_USED"),
+          port=os.getenv("DB_PORT")
+        )
+        self.cursor = self.db.cursor()
+
+    def insert_data(self, value):
+        """Записать тестовые данные в тестовую БД."""
+
+        sql = "INSERT INTO testdata (id, testdata) VALUES (%s, %s)"
+        val = (None, value)
+        self.cursor.execute(sql, val)
+        self.db.commit()
+        self.db.close()
+
+    def select_testdata_by_id(self, id_testdata=None):
+        """Выбрать тестовые данные по id записи в БД.
+        Если не передавать id, берется последняя запись в таблице.
+        """
+
+        query_result = {}
+        if id_testdata is None:
+            self.cursor.execute("SELECT id, testdata FROM testdata ORDER BY id DESC LIMIT 1")
+            for id, testdata in self.cursor:
+                query_result[id] = testdata
+
+        else:
+            self.cursor.execute(f"SELECT id, testdata FROM testdata WHERE id={id_testdata}")
+            for id, testdata in self.cursor:
+                query_result[id] = testdata
+
+        self.db.close()
+        return query_result
--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+#
 docker build -t project -f docker_build/project/Dockerfile .
 docker run \
 --name project-test \
@@ -10,11 +10,19 @@ project:latest
 
 docker exec project-test pip install -r requirements.txt
 
-#docker pull mysql:5.7.35
-#docker run \
-#-dit \
-#-p 10021:3306 \
-#-e MYSQL_ROOT_PASSWORD=123456789 \
-#-v "$(pwd)/database:/home/PoC/database" \
-#--name mysql \
-#mysql:5.7.35
+docker pull mysql:5.7.35
+docker run \
+-dit \
+-p 10021:3306 \
+-e MYSQL_ROOT_PASSWORD=123456789 \
+-v "$(pwd)/database:/var/lib/mysql" \
+--rm \
+--name mysql \
+mysql:5.7.35
+
+> .env
+echo DB_HOST=\"127.0.0.1\" >> .env
+echo DB_USER=\"root\" >> .env
+echo DB_PASSWORD=\"123456789\" >> .env
+echo DB_USED=\"test\" >> .env
+echo DB_PORT=\"10021\" >> .env
